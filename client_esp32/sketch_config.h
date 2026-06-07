63f1a2b7a35ec22baea67a5b5be67b9bc91ce637
Jon Fleming <jonfleming@hotmail.com>
2026-06-06 17:14:27 -0700

    freenove

--- client_esp32/sketch_config.h
+++ client_esp32/sketch_config.h
@@ -1,6 +1,7 @@
 #ifndef SKETCH_CONFIG_H
 #define SKETCH_CONFIG_H
 
+<<<<<<< Updated upstream
 // Board selection (uncomment ONE):
 //   BOARD_AIPI_LITE       - AIPI Lite (ST7735 SPI, ES8311 codec)
 //   BOARD_WAVESHARE_AMOLED - Waveshare ESP32-S3-Touch-AMOLED-1.8 (SH8601 QSPI AMOLED)
@@ -8,6 +9,10 @@
 
 // #define BOARD_AIPI_LITE
 // #define BOARD_WAVESHARE_AMOLED
+=======
+// Comment this out to build the same sketch for Freenove.
+//#define BOARD_AIPI_LITE
+>>>>>>> Stashed changes
 
 #ifndef ARDUINO_USB_CDC_ON_BOOT
 #define ARDUINO_USB_CDC_ON_BOOT 1
